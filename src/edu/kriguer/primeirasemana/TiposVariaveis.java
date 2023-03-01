98387c5dca8b41320b7afc42e12dd0f2762c9a43
Kriguer Freitas <kriguerfreitas2020@gmail.com>
2023-03-01 03:15:11 -0300

    Exercise: Variable and constant variables types with Java

---
+++ src/edu/kriguer/primeirasemana/TiposVariaveis.java
@@ -0,0 +1,25 @@
+package edu.kriguer.primeirasemana;
+
+public class TiposVariaveis {
+    public static void main(String[] args) {
+        // String/cadeia de caracteres;
+        String meuNome = "Kriguer Freitas";
+
+        // double/não-inteiros, podendo ser float com a convenção F ao final;
+        double salarioMinimo = 1320.00;
+
+        short numeroCurto = 1;
+        int numero = numeroCurto;
+        // sem o (SHORT) vai ocasionar um erro pois o numero sendo int,
+        // pode armazenar valores além da capacidade de short
+        short numeroCurto2 = (short) numero;
+
+        //Implementação de constantes
+        int x = 5;
+        x = 10; //Reatribuição permitida;
+        System.out.println(x);
+
+        final double PI = 3.14;
+        // PI = 10.75; // Reatribuição negada pois pi é uma constante ou "final";
+    }
+}
